5f5192336047cc0d620d4ed228d0bd0b24b448f2
Evgeniy Vostrov <evgeniy@binary.com>
2016-05-31 11:36:12 +0000

    Ended doc

--- lib/Mojolicious/Plugin/WebSocketProxy.pm
+++ lib/Mojolicious/Plugin/WebSocketProxy.pm
@@ -7,6 +7,8 @@ use Mojo::WebSocketProxy::Dispatcher;
 sub register {
     my ($self, $app, $config) = @_;
 
+    die 'No actions found!' unless $config->{base_path};
+
     my $url_getter;
     $url_getter = delete $config->{url} if $config->{url} and ref($config->{url}) eq 'CODE';
     $app->helper(
@@ -31,7 +33,7 @@ sub register {
             $dispatcher_config->add_action($actions->[$i], $i);
         }
     } else {
-        Carp::confess 'No actions found!';
+        die 'No actions found!';
     }
 
     return;
@@ -98,11 +100,16 @@ Or to manually call RPC server:
 
 Using this module you can forward websocket JSON-RPC 2.0 requests to RPC server.
 
+For every message it creates separate hash ref storage, which is available from
+hooks as $req_storage.
+Requset storage have RPC call parameters in $req_storage->{call_params}.
+It copy message args to $req_storage->{call_params}->{args}.
+
 The plugin understands the following parameters.
 
-=over
+=head1 Plugin parameters
 
-=item B<actions> (mandatory)
+=head2 actions
 
 A pointer to array of action details, which contain stash_params, 
 request-response callbacks, other call parameters.
@@ -115,7 +122,7 @@ request-response callbacks, other call parameters.
             ]
         });
         
-=item B<before_forward> (optional)
+=head2 before_forward
 
     before_forward => [sub { my ($c, $req_storage) = @_; ... }, sub {...}]
 
@@ -125,7 +132,7 @@ If returns any hash ref then that value will be JSON encoded and send to client,
 without forward action to RPC. To call RPC every hook should return empty or undefined value.
 It's good place to some validation or subscribe actions.
         
-=item B<after_forward> (optional)
+=head2 after_forward
     
     after_forward => [sub { my ($c, $result, $req_storage) = @_; ... }, sub {...}]
 
@@ -135,111 +142,118 @@ It can view or modify result value from 'before_forward' hook.
 It'll run every hook or until any hook returns some non-empty result.
 If returns any hash ref then that value will be JSON encoded and send to client.
 
-=item B<after_dispatch> (optional)
+=head2 after_dispatch
 
     after_dispatch => [sub { my $c = shift; ... }, sub {...}]
     
 Global hooks which will run at the end of request handling.
 
-=item B<before_get_rpc_response> (optional)
+=head2 before_get_rpc_response (global)
 
     before_get_rpc_response => [sub { my ($c, $req_storage) = @_; ... }, sub {...}]
 
 Global hooks which will run when asynchronous RPC call is answered.
 
-=item B<after_got_rpc_response> (optional)
+=head2 after_got_rpc_response (global)
 
     after_got_rpc_response => [sub { my ($c, $req_storage) = @_; ... }, sub {...}]
 
 Global hooks which will run after checked that response exists.
 
-=item B<before_send_api_response> (optional)
+=head2 before_send_api_response (global)
 
     before_send_api_response => [sub { my ($c, $req_storage, $api_response) = @_; ... }, sub {...}]
 
 Global hooks which will run immediately before send API response.
 
-=item B<after_sent_api_response> (optional)
+=head2 after_sent_api_response (global)
 
     before_send_api_response => [sub { my ($c, $req_storage) = @_; ... }, sub {...}]
 
 Global hooks which will run immediately after sent API response.
 
+=head2 base_path
 
+API url for make route.
 
+=head2 stream_timeout
 
+See L<Mojo::IOLoop::Stream/"timeout">
 
-=item B<success> (optional)
+=head2 max_connections
+
+See L<Mojo::IOLoop/"max_connections">
+
+=head2 opened_connection
+
+Callback for doing something once after connection is opened
+
+=head2 finish_connection
+
+Callback for doing something every time when connection is closed. 
+
+=head2 url
+
+RPC host url - store url getter function to get url dynamically for manually RPC calls.
+When using Forward call then url storing in request storage.
+You can store url in every action options, or make it at before_forward hook.
+
+=head1 Actions options
+
+=head2 stash_params
+
+    stash_params => [qw/ stash_key1 stash_key2 /]
+
+Will send specified parameters from Mojolicious $c->stash.
+You can store RPC response data to Mojolicious stash returning data like this:
+    
+    rpc_response => {
+        stast => {..} # data to store in Mojolicious stash
+        response_key1 => response_value1, # response to API client
+        response_key2 => response_value2
+    }
+
+=head2 success
 
     success => sub { my ($c, $rpc_response) = @_; ... }
     
 Hook which will run if RPC returns success value.
 
-=item B<error> (optional)
+=head2 error
 
     error => sub { my ($c, $rpc_response) = @_; ... }
     
 Hook which will run if RPC returns value with error key, e.g. 
 { result => { error => { code => 'some_error' } } }
 
-=item B<response> (optional)
+=head2 response
 
     response => sub { my ($c, $rpc_response) = @_; ... }
     
 Hook which will run every time when success or error callbacks is running.
 It good place to modify API response format.
 
+=head2 before_get_rpc_response
 
+Same as L<Mojolicious::Plugin::WebSocketProxy/"before_get_rpc_response (global)"> except for using for particular action
 
+=head2 after_got_rpc_response
 
+Same as L<Mojolicious::Plugin::WebSocketProxy/"after_got_rpc_response (global)"> except for using for particular action
 
+=head2 before_send_api_response
 
-            # main config
-            base_path         => '/websockets/v3',
-            stream_timeout    => 120,
-            max_connections   => 100000,
-            opened_connection => \&BOM::WebSocketAPI::Hooks::init_redis_connections,
-            finish_connection => \&BOM::WebSocketAPI::Hooks::forget_all,
+Same as L<Mojolicious::Plugin::WebSocketProxy/"before_send_api_response (global)"> except for using for particular action
 
-            # helper config
-            url =>
+=head2 after_sent_api_response
 
-        
-=back
+Same as L<Mojolicious::Plugin::WebSocketProxy/"after_sent_api_response (global)"> except for using for particular action
 
-=head1 METHODS
-
-=head2 open_connection
-
-Run while openning new wss connection.
-Run hook when connection is opened.
-Set finish connection callback.
-
-=head2 on_message
-
-Handle message - parse and dispatch request messages.
-Dispatching action and forward to RPC server.
-
-=head2 before_forward
-
-Run hooks.
-
-=head2 after_forward
-
-Run hooks.
-
-=head2 dispatch
-
-Dispatch request using message json key.
-
-=head2 forward
-
-Forward call to RPC server using global and action hooks.
-Don't forward call to RPC if any before_forward hook returns response.
-Or if there is instead_of_forward action.
-
-=head2 send_api_response
-
-Send asynchronous response to client websocket, doing hooks.
+=head1 SEE ALSO
+ 
+L<Mojolicious::Plugin::WebSocketProxy>, 
+L<Mojo::WebSocketProxy::Dispatcher::CallingEngine>,
+L<Mojo::WebSocketProxy::Dispatcher::Config>
+L<Mojo::WebSocketProxy::Dispatcher::Parser>
 
 =cut
